2430a2f478ebe130e34c1a69cc7f26d1cd4822c0
Gravestam <github@gravestam.site>
2024-04-22 14:18:50 +0200

    U

--- system/modules/boot.nix
+++ system/modules/boot.nix
@@ -7,5 +7,6 @@
 	boot.initrd.kernelModules = [ ];
 	boot.kernelModules = [ "kvm-amd" "nvidia" ];
 	boot.blacklistedKernelModules = [ "nouveau" ];
+	boot.kernelPackages = pkgs.linuxPackages_latest;
 	boot.extraModulePackages = [ ];
 }
--- system/modules/packages.nix
+++ system/modules/packages.nix
@@ -1,42 +1,52 @@
 { config, pkgs, pkgs-unstable, ... }:
 
 {
-	environment.systemPackages = [
-		pkgs.bat
-		pkgs.btop
-		pkgs.eza
-		pkgs.fzf
-		pkgs.git
-		pkgs.lm_sensors
-		pkgs.libsForQt5.qt5.qtquickcontrols2
-		pkgs.libsForQt5.qt5.qtgraphicaleffects
-		pkgs.libsForQt5.qt5.qtsvg
-		pkgs.fastfetch
-		pkgs.ripgrep
-		pkgs.tldr
-		pkgs.unzip
-		pkgs.openssl
-		pkgs.openssl.dev
-		pkgs.pkg-config
-		pkgs.wget
-		pkgs.xfce.thunar
-		pkgs.xdg-desktop-portal-gtk
-		pkgs.xdg-desktop-portal-wlr
-		pkgs.zip
-		pkgs.brave
-		pkgs.luajit
-		pkgs.go
-		pkgs.nodejs_21
-		pkgs.python3
-		pkgs.gcc_multi
-		pkgs.starship
-		pkgs.fd
-		pkgs.waybar
-		pkgs.rofi-wayland
-		pkgs.swww
-		pkgs.mako
-		pkgs.udiskie
+	environment.systemPackages = 
+	
+	(with pkgs; [
+		bat
+		btop
+		eza
+		fzf
+		git
+		lm_sensors
+		libsForQt5.qt5.qtquickcontrols2
+		libsForQt5.qt5.qtgraphicaleffects
+		libsForQt5.qt5.qtsvg
+		fastfetch
+		ripgrep
+		tldr
+		unzip
+		openssl
+		openssl.dev
+		pkg-config
+		wget
+		xfce.thunar
+		xdg-desktop-portal-gtk
+		xdg-desktop-portal-wlr
+		zip
+		brave
+		luajit
+		go
+		nodejs_21
+		python3
+		gcc_multi
+		starship
+		fd
+		waybar
+		rofi-wayland
+		swww
+		mako
+		udiskie
+		ranger
+		burpsuite
+		slack
+		brightnessctl
+	])
 
-		pkgs-unstable.alacritty
-	];
+	++
+
+	(with pkgs-unstable; [
+		alacritty
+	]);
 }
