3f5f85360686a43143fce9fd68884cfae20bf725
Gravestam <github@gravestam.site>
2024-04-23 11:10:59 +0200

    U

--- system/modules/dependencies.nix
+++ system/modules/dependencies.nix
@@ -3,12 +3,5 @@
 
 {
 	environment.systemPackages = [ 
-		pkgs-unstable.python312Packages.openai
-		pkgs-unstable.python312Packages.termcolor
-		pkgs-unstable.python312Packages.colorama
-		pkgs-unstable.python312Packages.python-dotenv
-		pkgs-unstable.python312Packages.distro
-		pkgs-unstable.python312Packages.pyyaml
-		pkgs-unstable.python312Packages.pyperclip
 	];
 }
