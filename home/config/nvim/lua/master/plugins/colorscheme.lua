863291ab674deda84dfa36f2f79d3f79db43e58c
Gravestam <github@gravestam.site>
2024-05-13 16:07:38 +0200

    U

--- home/config/nvim/lua/master/plugins/colorscheme.lua
+++ home/config/nvim/lua/master/plugins/colorscheme.lua
@@ -9,3 +9,17 @@ return {
 		vim.cmd("colorscheme tokyonight")
 	end,
 }
+
+-- return {
+-- 	"catppuccin/nvim",
+-- 	priority = 1000, -- make sure this loads asap...
+-- 	config = function()
+--
+-- 		require("catppuccin").setup({
+-- 			flavor = "mocha" -- latte, frappe, macchiato, mocha
+-- 		});
+--
+-- 		vim.cmd("colorscheme catppuccin")
+-- 	end,
+-- }
+
