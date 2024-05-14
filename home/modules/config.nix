b107d765e66a983b78ad2fec15aa17cb2cfe5b32
Gravestam <github@gravestam.site>
2024-05-14 22:02:41 +0200

    U

--- home/modules/config.nix
+++ home/modules/config.nix
@@ -9,6 +9,9 @@ in
     ".config/hypr/scripts".source = "${configdir}/hypr/scripts";
     ".config/swappy".source = "${configdir}/swappy";
     ".config/tmux".source = "${configdir}/tmux";
-    ".config/ranger".source = "${configdir}/ranger";
+    ".config/ranger/rifle.conf".source = "${configdir}/ranger/rifle.conf";
+    ".config/ranger/commands.py".source = "${configdir}/ranger/commands.py";
+    ".config/ranger/commands_full.py".source = "${configdir}/ranger/commands_full.py";
+    ".config/ranger/scope.sh".source = "${configdir}/ranger/scope.py";
   };
 }
