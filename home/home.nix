ba18e64de3db3df0feeed787557d48f46da908a1
Gravestam <github@gravestam.site>
2024-05-14 11:02:16 +0200

    U

--- home/home.nix
+++ home/home.nix
@@ -1,22 +1,22 @@
 { nix-colors, ... }:
 
 {
-	imports = [
-		nix-colors.homeManagerModules.default
-		./modules
-	];
+  imports = [
+    nix-colors.homeManagerModules.default
+    ./modules
+  ];
 
-	# colorScheme = nix-colors.colorSchemes.tokyo-night-dark;
-	# colorScheme = nix-colors.colorSchemes.tokyo-city-dark;
-	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-soft;
-	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium;
-	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
-	colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
-	# colorScheme = nix-colors.colorSchemes.nord;
+  # colorScheme = nix-colors.colorSchemes.tokyo-night-dark;
+  # colorScheme = nix-colors.colorSchemes.tokyo-city-dark;
+  # colorScheme = nix-colors.colorSchemes.gruvbox-dark-soft;
+  # colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium;
+  # colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
+  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+  # colorScheme = nix-colors.colorSchemes.nord;
 
-	home = {
-		username = "master";
-		homeDirectory = "/home/master";
-		stateVersion = "23.11";
-	};
+  home = {
+    username = "master";
+    homeDirectory = "/home/master";
+    stateVersion = "23.11";
+  };
 }
