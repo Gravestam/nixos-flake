925bb59c661a33ab484ec32797c7896693365fbe
Gravestam <github@gravestam.site>
2024-05-14 09:27:25 +0200

    U

--- home/config/nvim/lua/master/plugins/colorscheme.lua
+++ home/config/nvim/lua/master/plugins/colorscheme.lua
@@ -1,25 +1,25 @@
-return {
-	"folke/tokyonight.nvim",
-	priority = 1000, -- make sure this loads asap...
-	config = function()
-		require("tokyonight").setup({
-			style = "night",
-		})
-
-		vim.cmd("colorscheme tokyonight")
-	end,
-}
-
 -- return {
--- 	"catppuccin/nvim",
+-- 	"folke/tokyonight.nvim",
 -- 	priority = 1000, -- make sure this loads asap...
 -- 	config = function()
+-- 		require("tokyonight").setup({
+-- 			style = "night",
+-- 		})
 --
--- 		require("catppuccin").setup({
--- 			flavor = "mocha" -- latte, frappe, macchiato, mocha
--- 		});
---
--- 		vim.cmd("colorscheme catppuccin")
+-- 		vim.cmd("colorscheme tokyonight")
 -- 	end,
 -- }
+
+return {
+	"catppuccin/nvim",
+	priority = 1000, -- make sure this loads asap...
+	config = function()
+
+		require("catppuccin").setup({
+			flavor = "mocha" -- latte, frappe, macchiato, mocha
+		});
+
+		vim.cmd("colorscheme catppuccin")
+	end,
+}
 
--- home/home.nix
+++ home/home.nix
@@ -6,12 +6,12 @@
 		./modules
 	];
 
-	colorScheme = nix-colors.colorSchemes.tokyo-night-dark;
+	# colorScheme = nix-colors.colorSchemes.tokyo-night-dark;
 	# colorScheme = nix-colors.colorSchemes.tokyo-city-dark;
 	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-soft;
 	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium;
 	# colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
-	# colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+	colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
 	# colorScheme = nix-colors.colorSchemes.nord;
 
 	home = {
