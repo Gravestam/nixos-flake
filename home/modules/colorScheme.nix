e16d6447654e8cd5a1152f6154d4af35b006a6d4
Gravestam <github@gravestam.site>
2024-05-16 10:53:56 +0200

    U

--- home/modules/colorScheme.nix
+++ home/modules/colorScheme.nix
@@ -5,7 +5,7 @@
   # colorScheme = nix-colors.colorSchemes.tokyo-city-dark;
   # colorScheme = nix-colors.colorSchemes.gruvbox-dark-soft;
   # colorScheme = nix-colors.colorSchemes.gruvbox-dark-medium;
-  # colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
-  colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
+  colorScheme = nix-colors.colorSchemes.gruvbox-dark-hard;
+  # colorScheme = nix-colors.colorSchemes.catppuccin-mocha;
   # colorScheme = nix-colors.colorSchemes.nord;
 }
