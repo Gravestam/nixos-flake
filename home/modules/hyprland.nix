14a3ccecf8895b4c1baba838df7b9f378784726b
Gravestam <github@gravestam.site>
2024-11-21 20:59:42 +0100

    U

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -236,11 +236,6 @@ in
           new_optimizations = true;
           xray = true;
         };
-
-        drop_shadow = "yes";
-        shadow_range = 4;
-        shadow_render_power = 3;
-        "col.shadow" = "rgba(1a1a1aee)";
       };
 
       animations = {
