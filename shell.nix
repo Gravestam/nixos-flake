97cfc8625917888774e075a57c8c0ece1c7fbfa1
Gravestam <github@gravestam.site>
2025-01-13 09:21:15 +0100

    U

--- shell.nix
+++ shell.nix
@@ -1,6 +1,15 @@
-
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
 
+	nativeBuildInputs = with pkgs; [
+		python312
+		python312Packages.pip
+		python312Packages.pyautogui
+		python312Packages.keyboard
+	];
+
+	shellHook = ''
+		echo "DEV SHELL"
+	'';
 }
